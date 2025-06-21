b907a42574302806a0fef7f36326847910fc75c2
Arpit <arpitjoshi213.com>
2025-06-21 20:57:59 +0530

    added firebase logic

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,13 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyA3QUKhdyYfZR4-KtqMFk0QoGtlrLuL7nE",
-  authDomain: "lostandfound-9ffa4.firebaseapp.com",
-  projectId: "lostandfound-9ffa4",
-  storageBucket: "lostandfound-9ffa4.appspot.com",
-  messagingSenderId: "468156894826",
-  appId: "1:468156894826:web:60b9a18173731220abc413",
-  measurementId: "G-4FZ8MCYYS2"
-};
+    apiKey: "AIzaSyBLKwqpoV22o5J8q-xVbIjeGjziTWd9wek",
+    authDomain: "lost-9e29a.firebaseapp.com",
+    projectId: "lost-9e29a",
+    storageBucket: "lost-9e29a.firebasestorage.app",
+    messagingSenderId: "173946884129",
+    appId: "1:173946884129:web:a9aadcac9f16445568d360",
+    measurementId: "G-ZQG843PDPY"
+  };
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
